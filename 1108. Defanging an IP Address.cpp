6567f80a94149034676154fb8ddb381217347f67
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-10-24 00:55:47 +0600

    Add files via upload

---
+++ 1108. Defanging an IP Address.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string defangedAddress;
+        for (char c : address) {
+            if (c == '.') {
+                defangedAddress += "[.]";
+            } else {
+                defangedAddress += c;
+            }
+        }
+        return defangedAddress;
+    }
+};
